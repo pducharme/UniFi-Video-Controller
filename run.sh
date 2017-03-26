25371cdf1a80f19b490a27f8a5134fac2ff141b9
Donald Webster <fryfrog@gmail.com>
2017-03-25 19:19:17 -0700

    Switch to function for graceful shutdown and reduce sleep time.

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+function graceful_shutdown {
+  echo -n "Stopping unifi-video... " 
+  /usr/sbin/unifi-video --nodetach stop
+  if [[ $? -eq 0 ]]; then
+    echo "done."
+    exit 0
+  else
+    echo "failed."
+    exit 1
+  fi
+}
+
 # Trap SIGTERM for graceful exit
-trap 'echo -n "Stopping unifi-video... " ; /usr/sbin/unifi-video --nodetach stop ; echo "done." ; exit 0' SIGTERM
+trap graceful_shutdown SIGTERM
 
 # Run the unifi-video daemon the unifi-video way
 echo -n "Starting unifi-video... "
@@ -9,5 +21,5 @@ echo -n "Starting unifi-video... "
 echo "done."
 
 while true; do
-  sleep 5
+  sleep 1
 done
